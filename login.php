ee94984db0d04553e6ed4c0b5f60febe00fe2a16
Drachen_N01r <malfarizky23@gmail.com>
2023-08-15 11:23:46 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,14 +3,23 @@ include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $NPP = mysqli_real_escape_string($conn, $_POST['NPP']);
-    $password = mysqli_real_escape_string($conn, $_POST['pass']); 
+    $password = mysqli_real_escape_string($conn, $_POST['pass']);
+    $user_role = mysqli_real_escape_string($conn, $_POST['user_role']);
 
-    $sql = "SELECT * FROM users WHERE NPP='$NPP' AND password='$password'"; 
+    $sql = "SELECT * FROM users WHERE NPP='$NPP' AND password='$password' AND user_role='$user_role'"; 
 
     $result = mysqli_query($conn, $sql);
 
     if ($result && mysqli_num_rows($result) == 1) {
-        header("Location: dashboard_anggota.php");
+        $row = mysqli_fetch_assoc($result);
+        if ($row['user_role'] == 'Atasan') {
+            header("Location: dashboard_atasan.php");
+        } elseif ($row['user_role'] == 'Anggota') {
+            header("Location: dashboard_anggota.php");
+        } elseif ($row['user_role'] == 'Admin') {
+            header("Location: dashboard_admin.php");
+        }
+        exit;
     } else {
         echo "Username atau password salah!";
     }
