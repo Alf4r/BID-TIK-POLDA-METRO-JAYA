808648bf1d5d35871b13559bae8a159e95d7b5e9
Drachen_N01r <malfarizky23@gmail.com>
2023-08-16 08:35:42 +0700

    db

---
+++ database/polda_login (1).sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 16 Agu 2023 pada 03.34
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `polda_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `form_lap`
+--
+
+CREATE TABLE `form_lap` (
+  `id` int(11) NOT NULL,
+  `nama_lengkap` varchar(255) NOT NULL,
+  `no_nik` varchar(255) NOT NULL,
+  `korban` varchar(255) NOT NULL,
+  `tempat_lahir` varchar(255) NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `kewarganegaraan` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `pekerjaan` varchar(255) NOT NULL,
+  `alamat_lengkap` varchar(255) NOT NULL,
+  `lokasi_kejadian` varchar(255) NOT NULL,
+  `jenis_kejahatan` varchar(255) NOT NULL,
+  `kerugian` varchar(255) NOT NULL,
+  `tanggal_kejadian` date NOT NULL,
+  `waktu_kejadian` varchar(255) NOT NULL,
+  `detail_motif` varchar(255) NOT NULL,
+  `provinsi` varchar(255) NOT NULL,
+  `kabupaten` varchar(255) NOT NULL,
+  `kecamatan` varchar(255) NOT NULL,
+  `kode_Pos` varchar(255) NOT NULL,
+  `ibu` varchar(255) NOT NULL,
+  `bapak` varchar(255) NOT NULL,
+  `pekerjaan_ibu` varchar(255) NOT NULL,
+  `pekerjaan_bapak` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `NPP` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `NPP`, `password`, `user_role`) VALUES
+(1, '1', '1', 'Anggota'),
+(2, '2', '2', 'Atasan'),
+(3, 'admin', '1', 'Admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `form_lap`
+--
+ALTER TABLE `form_lap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `form_lap`
+--
+ALTER TABLE `form_lap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- form_lap2.php
+++ form_lap2.php
@@ -1,6 +1,47 @@
 <?php
 include 'config.php';
 
+if(isset($_POST['submit']))
+{
+    $nama_lengkap = $_POST['nama_lengkap'];
+    $no_nik = $_POST['no_nik'];
+    $korban = $_POST['korban'];
+    $tempat_lahir = $_POST['tempat_lahir'];
+    $tanggal_lahir = $_POST['tanggal_lahir']; 
+    $email = $_POST['email'];
+    $kewarganegaraan = $_POST['kewarganegaraan'];
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $agama = $_POST['agama']; 
+    $pekerjaan = $_POST['pekerjaan']; 
+    $alamat_lengkap = $_POST['alamat_lengkap']; 
+    $lokasi_kejadian = $_POST['lokasi_kejadian']; 
+    $jenis_kejahatan = $_POST['jenis_kejahatan']; 
+    $kerugian = $_POST['kerugian'];
+    $tanggal_kejadian = $_POST['tanggal_kejadian'];
+    $waktu_kejadian = $_POST['waktu_kejadian'];
+    $detail_motif = $_POST['detail_motif']; 
+    $provinsi = $_POST['provinsi']; 
+    $kecamatan = $_POST['kecamatan']; 
+    $kode_pos = $_POST['kode_pos'];
+    $ibu = $_POST['ibu'];
+    $bapak = $_POST['bapak']; 
+    $pekerjaan_ibu = $_POST['pekerjaan_ibu']; 
+    $pekerjaan_bapak = $_POST['pekerjaan_bapak'];
+    $sql=mysqli_query($con,"select id from users where email='$email'");
+    $row=mysqli_num_rows($sql);
+    if($row>0)
+    {
+        echo "<script>alert('Email id already exist with another account. Please try with other email id');</script>";
+    }else{
+        $msg=mysqli_query($con,"insert into form_lap (nama_lengkap, no_nik, korban, tempat_lahir, tanggal_lahir, email, kewarganegaraan, jenis_kelamin, agama, pekerjaan, alamat_lengkap, lokasi_kejadian, jenis_kejahatan, kerugian, tanggal_kejadian, waktu_kejadian, detail_motif, provinsi, kabupaten, kecamatan, kode_pos, ibu, bapak, pekerjaan_ibu, pekerjaan_bapak) 
+                    values('$nama_lengkap', '$no_nik', '$korban', '$tempat_lahir', '$tanggal_lahir', '$email', '$kewarganegaraan', '$jenis_kelamin', '$agama', '$pekerjaan', '$alamat_lengkap', '$lokasi_kejadian', '$jenis_kejahatan', '$kerugian', '$tanggal_kejadian', '$waktu_kejadian', '$detail_motif', '$provinsi', '$kabupaten', '$kecamatan', '$kode_pos', '$ibu', '$bapak', '$pekerjaan_ibu', '$pekerjaan_bapak')");
+
+        if($msg)
+        {
+            header("Location:   dashboard_anggota.php");
+        }
+    }
+}
 $nama_lengkap = isset($_POST['nama_lengkap']) ? mysqli_real_escape_string($conn, $_POST['nama_lengkap']) : '';
 $no_nik = isset($_POST['no_nik']) ? mysqli_real_escape_string($conn, $_POST['no_nik']) : '';
 $korban = isset($_POST['korban']) ? mysqli_real_escape_string($conn, $_POST['korban']) : '';
