f83eec2892745f2a2df37848462572f01f9b84fa
rifkyfirdaus28 <rifkyfrds@student.telkomuniversity.ac.id>
2023-08-18 11:20:12 +0700

    tambah table au

--- dashboard_atasan.php
+++ dashboard_atasan.php
@@ -65,15 +65,9 @@ echo "Selamat datang di Dashboard Atasan, " . $_SESSION['login_user'] . "!";
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <ul class="navbar-nav  ">
+            <ul class="navbar-nav  ">
             <li class="nav-item active">
                 <a class="nav-link" href="index.html">Beranda <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="service.html">Komparasi</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Formulir_Laporan1.php">Formulir Laporan</a>
             </li>
           </ul>
           <form class="">
@@ -81,9 +75,7 @@ echo "Selamat datang di Dashboard Atasan, " . $_SESSION['login_user'] . "!";
           </form>
         </div>
     </nav>
-    <div style="width:100%;height:400px;">
-        <img class="center" src="img/Lambang_Polri.png" style="display: block;margin-right: auto;margin-left: auto;margin-top: 218px;">
-        <div class="position-absolute top-0 start-0 w-100  d-flex align-items-center" style="background: rgba(247, 247, 247); opacity: 0.9; height: 129%;">
-    </div>
+    <div class='tableauPlaceholder' id='viz1692332067092' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;IndonesianCrimeDashboard2022&#47;Dashboard&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='IndonesianCrimeDashboard2022&#47;Dashboard' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;In&#47;IndonesianCrimeDashboard2022&#47;Dashboard&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1692332067092');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+    
 </body>
 </html>
